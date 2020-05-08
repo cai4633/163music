305c8268d7cb321162899a512bf007c8a07ea1e7
cai4633 <463353522@qq.com>
2020-05-08 13:46:42 +0800

    首页获取音乐列表，优化首页界面

--- src/js/index/latestSong.js
+++ src/js/index/latestSong.js
@@ -1,54 +1,61 @@
 {
   let view = {
-    el: '.latest-song',
-    init(){this.$el = $(this.el)}, 
+    el: ".latest-song",
+    init() {
+      this.$el = $(this.el)
+    },
     template: `
     <h1>最新音乐</h1>
-    <ul>
-      <li>
-        <div class="left">
-          <h2>暗示暗示暗示暗示暗暗示暗示暗暗示暗示暗暗示暗示暗示</h2>
-          <div class="text"><span class='sq'></span><span class="details">周鑫-暗示</span></div>
-        </div>
-        <div class="right">
-          <span class="play">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-play"></use>
-            </svg>
-          </span>
-        </div>
-      </li>
-      <li></li>
-
-      <li>
-        <div class="left">
-          <h2>暗示暗示暗示暗示暗暗示暗示暗暗示暗示暗暗示暗示暗示</h2>
-          <div class="text"><span>SQ</span><span class="details">周鑫-暗示</span></div>
-        </div>
-        <div class="right">
-          <span class="play">
-            <svg class="icon" aria-hidden="true">
-              <use xlink:href="#icon-play"></use>
-            </svg>
-          </span>
-        </div>
-      </li>
-      <li>
-    </ul>
+    <ul>{{contents}}</ul>
   `,
-    render(data){
-      this.$el.html(this.template)
-    }
+    li: `
+    <li>
+    <div class="left">
+      <h2>{{song_name}}</h2>
+      <div class="text"><span class='sq'></span><span class="details">{{singer}}-{{song_name}}</span></div>
+    </div>
+    <div class="right">
+      <span class="play">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#icon-play"></use>
+        </svg>
+      </span>
+    </div>
+  </li>`,
+    render(data) {
+      let songs = data.songs
+      let keys = ['song_name','singer']
+      let texts = []
+      songs.forEach((song)=>{
+        let text = this.li
+        keys.forEach((str)=>{ text = text.replace(new RegExp(`{{${str}}}`,'g'), song[`${str}`]) })
+        texts.push(text)
+      })
+      let html = this.template.replace('{{contents}}',texts.join(''))
+      this.$el.html(html)
+    },
   }
-  let model = {}
+  let model = {
+    data: { songs: [] },
+    getAllList() {
+      const query = new AV.Query("Song")
+      return query.find().then((songs) => {
+        songs.forEach((song) => {
+          this.data.songs.push({ id: song.id, ...song.attributes })
+        })
+      })
+    },
+  }
   let controller = {
-    init(view, model){
+    init(view, model) {
       this.view = view
       this.model = model
       this.view.init()
-      this.view.render(this.model.data)
-
-    }
+      this.model.getAllList().then(() => {
+        console.log(this.model.data)
+        this.view.render(this.model.data)
+      })
+    },
   }
   controller.init(view, model)
 }
